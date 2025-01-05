91229b63240253d8d466c66a64da58a2f0fa8a74
Dal Bdr Rai <dal123rai@gmail.com>
2025-01-06 09:59:45 +1100

    Code with error

--- lib/file_with_error.rb
+++ lib/file_with_error.rb
@@ -1,6 +1,8 @@
-class VulnerableClass
-  def execute_code(user_input)
-    # Intentionally vulnerable code
-    eval(user_input) # This allows execution of arbitrary code, which is unsafe
+class SecretHandler
+  # Hardcoded secret (this is bad practice)
+  API_KEY = "hardcoded-secret-api-key"
+
+  def print_api_key
+    puts "The API key is: #{API_KEY}"
   end
 end
